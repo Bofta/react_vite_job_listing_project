2d8520924a0b0d03d125f401c8b23f24fc108457
Bofta <montassar367@gmail.com>
2024-08-19 12:17:46 +0200

    React Jobs Listing Demo

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { 
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider 
+} from 'react-router-dom';
+
+import MainLayout from './layouts/MainLayout';
+import Homepage from './pages/Homepage';
+import JobsPage from './pages/JobsPage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<Homepage />}/>
+      <Route path='/jobs' element={<JobsPage />}/>
+      
+    </Route>
+  )
+);
+
+
+function App() {
+  return <RouterProvider router={router} />
+};
+
+export default App;
---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,9 @@
+const JobsPage = () => {
+  return (
+    <div>
+       Welcome to Jobs Page
+    </div>
+  )
+}
+
+export default JobsPage
